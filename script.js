59d237e062a8f31dc1aaf278594dd4d727d89b12
Anusha-Panchumarthi <anusha.panchumarthi@gmail.com>
2022-03-06 11:30:08 +0530

    second

---
+++ script.js
@@ -0,0 +1,15 @@
+var msg_txt = "Welcome to the Metaverse";
+var speed = 75;
+var i=0;
+function Typing(){
+    if(i<msg_txt.length){
+        document.getElementById('msg').innerHTML+=msg_txt.charAt(i);
+        i++;
+        console.log(i)
+        
+        setTimeout(Typing, speed);
+    }
+
+}
+
+Typing()
